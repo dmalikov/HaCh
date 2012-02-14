0b724778ea3789c6d21cb109b45d61f0acc77ade
Matvey Aksenov <matvey.aksenov@gmail.com>
2012-02-14 22:01:49 +0400

    Add several clients support.

--- Server.hs
+++ Server.hs
@@ -1,7 +1,8 @@
 module Main where
 
 import Control.Monad (forever, unless, void)
-import Network
+import Control.Concurrent (forkIO)
+import Network.Socket
 import System.IO
 
 loop :: Handle -> IO ()
@@ -9,9 +10,17 @@ loop h = do r <- hGetLine h
             unless (null r) (void $ hPutStrLn h r)
             loop h
 
+serve :: Socket -> IO ()
+serve sock = do (s, _) <- accept sock
+                h <- socketToHandle s ReadWriteMode
+                hSetBuffering h LineBuffering
+                forkIO (loop h)
+                serve sock
+
 main :: IO ()
 main = withSocketsDo $ do
-         socket <- listenOn (PortNumber 7123)
-         (h, _, _) <- accept socket
-         hSetBuffering h LineBuffering
-         forever (loop h)
+         sock <- socket AF_INET Stream 0
+         setSocketOption sock ReuseAddr 1
+         bindSocket sock (SockAddrInet 7123 iNADDR_ANY)
+         listen sock 1024
+         serve sock
