babc9325a168d396d41f1be8512f063d0307135c
Dmitry Malikov <color404051h@gmail.com>
2012-05-15 00:29:08 +0400

    Server: add ClientId

--- Server.hs
+++ Server.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE BangPatterns #-}
 module Main where
 
+import Control.Exception
 import Control.Monad (forever)
 import Control.Concurrent (forkIO)
 import Control.Concurrent.Chan
@@ -9,29 +10,35 @@ import System.IO
 
 import Types
 
-readC :: Chan (Int, Message) -> Handle -> Int -> IO ()
-readC ch h _ = do (_, m) <- readChan ch
-                  hPrint h m
+readC :: Chan (Int, Message) -> Handle -> IO ()
+readC ch h = do
+  (_, m) <- readChan ch
+  hPrint h m
 
-client :: Chan (Int, Message) -> Handle -> Int -> IO ()
-client ch h n = do ch' <- dupChan ch
-                   forkIO (forever $ readC ch' h n)
-                   forever $ do m <- hGetLine h
-                                writeChan ch' (n, read m)
+client :: Chan (Int, Message) -> Handle -> ClientId -> IO ()
+client ch h (ClientId n) = do
+  ch' <- dupChan ch
+  forkIO (forever $ readC ch' h)
+  handle onExit $ forever $ do
+    m <- hGetLine h
+    writeChan ch' (n, read m)
+  where onExit :: SomeException -> IO ()
+        onExit e = print e
 
-serve :: Socket -> Chan (Int, Message) -> Int -> IO ()
-serve sock ch !n = do (s, _) <- accept sock
-                      h <- socketToHandle s ReadWriteMode
-                      hSetBuffering h LineBuffering
-                      forkIO $ client ch h n
-                      serve sock ch (n+1)
+serve :: Socket -> Chan (Int, Message) -> ClientId -> IO ()
+serve sock ch (ClientId !n) = do
+  (s, _) <- accept sock
+  h <- socketToHandle s ReadWriteMode
+  hSetBuffering h LineBuffering
+  forkIO $ client ch h (ClientId n)
+  serve sock ch (ClientId $ n+1)
 
 main :: IO ()
 main = withSocketsDo $ do
-         sock <- socket AF_INET Stream 0
-         setSocketOption sock ReuseAddr 1
-         bindSocket sock (SockAddrInet 7123 iNADDR_ANY)
-         listen sock 1024
-         ch <- newChan
-         forkIO $ forever $ readChan ch >>= const (return ())
-         serve sock ch 0
+  sock <- socket AF_INET Stream 0
+  setSocketOption sock ReuseAddr 1
+  bindSocket sock (SockAddrInet 7123 iNADDR_ANY)
+  listen sock 1024
+  ch <- newChan
+  forkIO $ forever $ readChan ch >>= const (return ())
+  serve sock ch (ClientId 0)
--- Types.hs
+++ Types.hs
@@ -1,7 +1,7 @@
 module Types where
 
-data Message = Message Type Nick Text deriving (Show, Read)
-data Type = Plain | Action | System deriving (Show, Read)
-newtype Nick = Nick String deriving (Show, Read)
-newtype Text = Text String deriving (Show, Read)
-
+newtype ClientId = ClientId Int deriving (Eq, Ord, Read, Show)
+data Message = Message Type Nick Text deriving (Read, Show)
+data Type = Plain | Action | System deriving (Read, Show)
+newtype Nick = Nick String deriving (Read, Show)
+newtype Text = Text String deriving (Read, Show)
