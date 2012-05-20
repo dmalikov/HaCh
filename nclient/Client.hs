9c1d5fb8dc48b71894de072216b64a563f87cdfa
Matvey Aksenov <matvey.aksenov@gmail.com>
2012-05-20 19:03:06 +0400

    Add TUI client skeleton.

---
+++ nclient/Client.hs
@@ -0,0 +1,68 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE UnicodeSyntax #-}
+module Main (main) where
+
+import Control.Applicative ((<$>))
+import Control.Concurrent (forkIO)
+import Control.Concurrent.Chan (Chan, newChan, readChan, writeChan)
+import Control.Exception (SomeException, catch, handle)
+import Control.Monad (forever, void)
+import Data.List (isPrefixOf)
+import Data.Time.Clock (getCurrentTime)
+import Data.Time.Format (formatTime)
+import Graphics.Vty.Widgets.All
+import Network
+import Prelude hiding (catch)
+import System.Environment (getArgs)
+import System.Exit (exitFailure, exitSuccess)
+import System.Locale (defaultTimeLocale)
+import System.IO (hFlush, hGetLine, hPrint, hPutStrLn, hSetBuffering, BufferMode(LineBuffering))
+import Text.Printf (printf)
+
+import Hach.Types
+
+import NClient.Args
+
+type Input = Chan String
+type Output = Chan String
+
+main ∷ IO ()
+main = do
+  (ip, nick) ← parseArgs =<< getArgs
+  i ← newChan
+  o ← newChan
+  initClient i o ip nick
+  writeChan o . show $ CMessage "EBLO"
+  forever $ return ()
+  --gui i o
+
+initClient ∷ Input → Output → String → String → IO ()
+initClient i o ip nick = do
+  withSocketsDo . void $
+    do h ← connectTo ip $ PortNumber 7123
+       hSetBuffering h LineBuffering
+       hPrint h $ CSetNick nick
+       forkIO $ catch (inputThread h) $ \(_ ∷ SomeException) → do
+         putStrLn "Server has closed the connection."
+         exitFailure
+       forkIO $ catch (outputThread h) $ \(_ ∷ SomeException) → do
+         putStrLn $ nick ++ " has left."
+         exitSuccess
+  where inputThread h = forever $ hGetLine h >>= writeChan i . read
+        outputThread h = forever $ readChan o >>= \m → print m >> hPrint h m
+
+gui ∷ Input → Output → IO ()
+gui i o = do
+  messages ← editWidget
+  newMessage ← editWidget
+  box ← vBox messages newMessage
+  ui ← centered box
+  fg ← newFocusGroup
+  addToFocusGroup fg newMessage
+  c ← newCollection
+  addToCollection c ui fg
+  newMessage `onActivate` \this → do
+    t ← getEditText this
+    writeChan o . show $ CMessage t
+    setEditText this " "
+  runUi c defaultContext
---
+++ nclient/NClient/Args.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE UnicodeSyntax #-}
+module NClient.Args (parseArgs) where
+
+import System.Console.GetOpt
+
+data Options = ServerIP String
+             | Name String
+
+options ∷ [OptDescr Options]
+options =
+  [ Option "s" ["server"] (ReqArg ServerIP "Server IP") "Set server IP address."
+  , Option "n" ["nick"] (ReqArg Name "User nickname") "Set user nickname."
+  ]
+
+parseArgs ∷ [String] → IO (String, String)
+parseArgs argv = case getOpt Permute options argv of
+  (os, _, []) →
+    let ips = [ s | ServerIP s ← os ]
+        nicks = [ n | Name n ← os ]
+    in case (ips, nicks) of
+      ([ip], [nick]) → return (ip, nick)
+      (_, _) → error $ usageInfo usage options
+  (_, _, es) → error $ concat es ++ usageInfo usage options
+  where usage = "Usage: hach-nclient [OPTIONS]"
+
