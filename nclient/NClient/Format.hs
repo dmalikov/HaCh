73444e66251925b850e8a235e595525a342590e9
Matvey Aksenov <matvey.aksenov@gmail.com>
2012-05-23 02:59:44 +0400

    Cosmetics.

--- nclient/NClient/Format.hs
+++ nclient/NClient/Format.hs
@@ -8,21 +8,21 @@ import Data.Char (isSpace)
 import Data.Time.Clock (getCurrentTime)
 import Data.Time.Format (formatTime)
 import Hach.Types
-import System.Exit (exitFailure, exitSuccess)
+import System.Exit (exitSuccess)
 import System.Locale (defaultTimeLocale)
 import Text.Printf (printf)
 
 fromS2C ∷ S2C → IO String
-fromS2C m = formatMessage m . formatTime defaultTimeLocale timeFormat <$> getCurrentTime
+fromS2C m = formatMessage . formatTime defaultTimeLocale timeFormat <$> getCurrentTime
   where timeFormat = "%H:%M:%S"
         messageFormat (SMessage n _) = "[%s] <" ++ n ++ ">: %s\n"
         messageFormat (SAction n _) = "[%s] *" ++ n ++ " %s\n"
         messageFormat (SSetNick n _) = "[%s] "  ++ n ++ " %s\n"
         messageFormat (SSystem _) = "[%s] ! %s\n"
-        formatMessage m t = printf (messageFormat m) t (text m)
+        formatMessage t = printf (messageFormat m) t (text m)
 
 toC2S ∷ String → IO C2S
-toC2S (format → ("/exit", t)) = exitSuccess
+toC2S (format → ("/exit", _)) = exitSuccess
 toC2S (format → ("/nick", t)) = return $ CSetNick t
 toC2S (format → ("/me", t)) = return $ CAction t
 toC2S t = return . CMessage . reverse . drop 1 . reverse $ t
