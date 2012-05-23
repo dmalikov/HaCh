ff2bd97c78e4eebdf509d154b4e8940ccb27a07c
Matvey Aksenov <matvey.aksenov@gmail.com>
2012-05-23 15:25:43 +0400

    Hacky word message splitting algorithm is added.

--- nclient/NClient/Message/Split.hs
+++ nclient/NClient/Message/Split.hs
@@ -2,7 +2,9 @@
 {-# LANGUAGE ViewPatterns #-}
 module NClient.Message.Split (simple, words) where
 
+import Data.List (intercalate)
 import Prelude hiding (words)
+import qualified Prelude
 
 simple ∷ Integral α ⇒ α → String → [String]
 simple (fromIntegral → n) = go
@@ -11,5 +13,15 @@ simple (fromIntegral → n) = go
           | otherwise = let (a,b) = splitAt n xs
                         in a : go b
 
-words ∷ Int → String → [String]
-words = undefined
+words ∷ Integral α ⇒ α → String → [String]
+words (fromIntegral → n) = map (intercalate " " . reverse) . go [] . Prelude.words
+  where go ∷ [String] → [String] → [[String]]
+        go [] [] = []
+        go [] (x:xs)
+          | length x < n = go [x] xs
+          | otherwise = let (a,b) = splitAt n x
+                        in [a] : go [] (b:xs)
+        go a [] = [a]
+        go a (x:xs)
+          | length a + sum (map length a) + length x <= n = go (x:a) xs
+          | otherwise = a : go [] (x:xs)
